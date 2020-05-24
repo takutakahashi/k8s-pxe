2bee3752ee84e54a1fc815f6364ce91e183e5a53
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-05-25 00:29:45 +0900

    pull

--- files/bin/register_dns.sh
+++ files/bin/register_dns.sh
@@ -2,4 +2,5 @@
 
 SERIAL=`cat /proc/cpuinfo |grep -i serial |awk '{print $3}'`
 ADDR=`ip a |grep eth0 |grep inet |awk '{print $2}' |awk -F'/' '{print $1}'`
+docker pull takutakahashi/route53-registerer
 docker run -it --env-file=/etc/registerer/env takutakahashi/route53-registerer ${SERIAL:8}.lab.takutakahashi.dev A $ADDR
--- files/bin/register_secret.sh
+++ files/bin/register_secret.sh
@@ -4,5 +4,5 @@ while [[ ! -f "/var/lib/rancher/k3s/server/token" ]];
 do
   sleep 1
 done
-
+docker pull takutakahashi/route53-registerer
 docker run -it --env-file=/etc/registerer/env takutakahashi/route53-registerer `hostname` TXT \\\"`cat /var/lib/rancher/k3s/server/token`\\\"
