bc270ea1d2b16546ab26615e4e296339603e64dc
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-05-24 23:24:22 +0900

    enable

--- bootstrap/bin/startup.sh
+++ bootstrap/bin/startup.sh
@@ -5,4 +5,6 @@ apt install -y git
 git clone https://github.com/takutakahashi/k8s-pxe.git /tmp/pxe
 cd /tmp/pxe/files
 find . -type f |xargs -I{} mv {} /{}
+cd etc/systemd/system
+find . -type f |xargs -I{} ln -s /etc/systemd/system/{} /etc/systemd/system/multi-user.target.wants/{}
 /bin/provision.sh
