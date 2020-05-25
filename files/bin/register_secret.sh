150386287da7cbf4e69f3ef071f9179c7656b92c
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-05-25 21:42:45 +0900

    mod to k8s

--- files/bin/register_secret.sh
+++ files/bin/register_secret.sh
@@ -7,4 +7,4 @@ done
 ADDR=`ip a |grep eth0 |grep inet |awk '{print $2}' |awk -F'/' '{print $1}'`
 docker pull takutakahashi/route53-registerer
 docker run --env-file=/etc/registerer/env takutakahashi/route53-registerer k8s.lab.takutakahashi.dev A $ADDR
-docker run --env-file=/etc/registerer/env takutakahashi/route53-registerer `hostname` TXT \\\"`cat /var/lib/rancher/k3s/server/token`\\\"
+docker run --env-file=/etc/registerer/env takutakahashi/route53-registerer k8s.lab.takutakahashi.dev TXT \\\"`cat /var/lib/rancher/k3s/server/token`\\\"
