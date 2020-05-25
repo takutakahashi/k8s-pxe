7180b480128b87b052f748f2d4925486f0497073
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-05-25 21:23:08 +0900

    use k8s.lab

--- files/bin/k3s_up.sh
+++ files/bin/k3s_up.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -xe
-ls /bin/k3s && ls /var/lib/rancher/k3s && k3s server --node-taint k3s-controlplane=true:NoExecute
+
+K3S_OPTS="--node-taint k3s-controlplane=true:NoExecute --no-deploy traefik"
+ls /bin/k3s && ls /var/lib/rancher/k3s && k3s server $K3S_OPTS
 apt install -y dnsutils netcat
 wget "https://github.com/rancher/k3s/releases/download/v1.16.9%2Bk3s1/k3s-armhf"
 chmod +x k3s-armhf
 mv k3s-armhf /bin/k3s
-SERIAL=`cat /proc/cpuinfo |grep -i serial |awk '{print $3}'`
-test "$SERIAL" = "" && SERIAL="aaaaaaaaaaaaaaa"
-dig +short SRV k3s-master.lab.takutakahashi.dev |grep -v "$SERIAL"|awk '{print $4}' |while read host
+host=k8s.lab.takutakahashi.dev
+for i in 1 2 3 4 5
 do
-  for i in 1 2 3 4 5
-  do
-    echo exit |nc -w 10 ${host} 6443 && ( K3S_TOKEN=`dig +short TXT ${host} | sed 's/"//g'` k3s server --server https://${host}:6443 --node-taint k3s-controlplane=true:NoExecute) || true
-  done
+  echo exit |nc -w 10 $host 6443 && ( K3S_TOKEN=`dig +short TXT ${host} | sed 's/"//g'` k3s server --server https://${host}:6443 $K3S_OPTS) || true
 done
 
-k3s server --cluster-init --tls-san k8s.lab.takutakahashi.dev --node-taint k3s-controlplane=true:NoExecute
+register_secret.sh &
+k3s server --cluster-init --tls-san k8s.lab.takutakahashi.dev $K3S_OPTS
