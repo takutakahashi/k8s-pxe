1a4883757282a64011eddd34937e2fb45dd89ca0
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-06-14 17:32:27 +0900

    rm -f

--- files/bin/k3s_up.sh
+++ files/bin/k3s_up.sh
@@ -3,7 +3,7 @@
 K3S_OPTS="--node-taint k3s-controlplane=true:NoExecute --no-deploy traefik"
 ls /bin/k3s && ls /var/lib/rancher/k3s && k3s server $K3S_OPTS
 apt install -y dnsutils netcat
-rm /bin/k3s
+rm -f /bin/k3s
 wget "https://github.com/rancher/k3s/releases/download/v1.17.5%2Bk3s1/k3s-armhf" -O /bin/k3s
 chmod +x /bin/k3s
 host=k8s.lab.takutakahashi.dev
