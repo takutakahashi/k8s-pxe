eb4c2846fee7b8ad238d3ca93dc298a899917ed7
takutaka <takutaka@pepabo.com>
2020-06-20 00:46:29 +0900

    enable ssh

--- files/bin/provision.sh
+++ files/bin/provision.sh
@@ -4,6 +4,13 @@
 SERIAL=`cat /proc/cpuinfo |grep -i serial |awk '{print $3}'`
 hostname ${SERIAL:8}.lab.takutakahashi.dev
 
+### enable ssh ###
+systemctl enable ssh
+mkdir /home/pi/.ssh
+curl https://github.com/takutakahashi.keys > /home/pi/.ssh/authorized_keys
+chown pi:pi -R /home/pi/.ssh
+chmod 644 /home/pi/.ssh/authorized_keys
+
 ### docker install ###
 apt update -y
 apt install -y docker.io
