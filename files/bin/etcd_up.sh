32a44dd7f1d9452d41f5ddc8c695dbaf7f91cf30
takutaka <takutaka@pepabo.com>
2020-06-28 15:33:38 +0900

    mod etcd_up.sh

--- files/bin/etcd_up.sh
+++ files/bin/etcd_up.sh
@@ -2,8 +2,13 @@
 ls /bin/etcd || /bin/etcd_build.sh
 HOSTNAME=`hostname`
 IP=`dig +short $HOSTNAME`
-ETCD_UNSUPPORTED_ARCH=arm /bin/etcd --name `hostname` --initial-advertise-peer-urls http://$IP:2380 \
-  --listen-peer-urls http://$IP:2380 \
-  --listen-client-urls http://$IP:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://$IP:2379 \
-  --discovery https://discovery.etcd.io/5586cb47489459c0ca8c025d42bee89f
+ETCD_UNSUPPORTED_ARCH=arm /bin/etcd --name $HOSTNAME  --data-dir /var/lib/etcd/$HOSTNAME \
+  --cert-file=/etc/etcd/certs/server.crt --key-file=/etc/etcd/certs/server.key \
+  --peer-auto-tls \
+  --discovery-srv lab.takutakahashi.dev \
+  --initial-advertise-peer-urls https://$HOSTNAME:2380 \
+  --initial-cluster-token etcd-cluster-1 \
+  --initial-cluster-state new \
+  --advertise-client-urls https://$HOSTNAME:2379 \
+  --listen-client-urls https://$IP:2379,https://127.0.0.1:2379 \
+  --listen-peer-urls https://$IP:2380
