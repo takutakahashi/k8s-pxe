4f8e7199cc1b535bc11ad1f50aaf28935ef51479
takutaka <takutaka@pepabo.com>
2020-07-03 23:29:05 +0900

    add files

--- bootstrap/bin/startup.sh
+++ bootstrap/bin/startup.sh
@@ -5,6 +5,7 @@ apt update -y
 apt install -y git
 git clone https://github.com/takutakahashi/k8s-pxe.git /tmp/pxe
 cd /tmp/pxe/files
+find -type d |xargs -I{} mkdir -p /{}
 find . -type f |xargs -I{} mv {} /{}
 cd etc/systemd/system
 find . -type f |xargs -I{} ln -s /etc/systemd/system/{} /etc/systemd/system/multi-user.target.wants/{}
--- files/bin/mkcert.sh
+++ files/bin/mkcert.sh
@@ -2,6 +2,6 @@
 HOSTNAME=`hostname`
 IP=`dig +short $HOSTNAME`
 ls /bin/cfssl || cfssl_build.sh
-cd /tmp/pxe/files/bin/cfssl
+cd /etc/cfssl
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 echo '{"CN":"k8s.lab.takutakahashi.dev","hosts":[""],"key":{"algo":"rsa","size":2048}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="$IP,$HOSTNAME,localhost,127.0.0.1" - | cfssljson -bare server
