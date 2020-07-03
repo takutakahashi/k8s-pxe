4a59d3fdbbf6a7eb408c26b869d2f4078010a9d8
takutaka <takutaka@pepabo.com>
2020-07-03 22:15:59 +0900

    add localhost

--- files/bin/mkcert.sh
+++ files/bin/mkcert.sh
@@ -2,4 +2,4 @@
 wget -O /bin/mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-linux-arm
 chmod +x /bin/mkcert
 mkcert -install
-srv2ip.sh _etcd-client-ssl._tcp.lab.takutakahashi.dev |xargs mkcert -cert-file /etc/etcd/certs/server.crt -key-file /etc/etcd/certs/cerver.key lab.takutakahashi.dev "*.lab.takutakahashi.dev"
+srv2ip.sh _etcd-client-ssl._tcp.lab.takutakahashi.dev |xargs mkcert -cert-file /etc/etcd/certs/server.crt -key-file /etc/etcd/certs/cerver.key lab.takutakahashi.dev "*.lab.takutakahashi.dev" localhost 127.0.0.1
