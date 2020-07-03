d7c61f278815d9eb7cb8e6ff9c0caf2cf22b7538
takutaka <takutaka@pepabo.com>
2020-07-04 01:51:06 +0900

    mod etcd_up

--- files/bin/etcd_up.sh
+++ files/bin/etcd_up.sh
@@ -3,12 +3,10 @@ ls /bin/etcd || /bin/etcd_build.sh
 HOSTNAME=`hostname`
 IP=`dig +short $HOSTNAME`
 ETCD_UNSUPPORTED_ARCH=arm /bin/etcd --name $HOSTNAME  --data-dir /var/lib/etcd/$HOSTNAME \
-  --cert-file=/etc/cfssl/server.pem --key-file=/etc/cfssl/server-key.pem --trusted-ca-file=/etc/cfssl/ca.pem \
-  --peer-cert-file=/etc/cfssl/server.pem --peer-key-file=/etc/cfssl/server-key.pem --peer-trusted-ca-file=/etc/cfssl/ca.pem \
-  --discovery-srv lab.takutakahashi.dev \
   --initial-advertise-peer-urls https://$HOSTNAME:2380 \
   --initial-cluster-token etcd-cluster-1 \
   --initial-cluster-state new \
+  --initial-cluster f903c77f.lab.takutakahashi.dev=http://10.10.1.14:2380,dd03570a.lab.takutakahashi.dev=http://10.10.1.15:2380,2dff6fc0.lab.takutakahashi.dev=http://10.10.1.27:2380 \
   --advertise-client-urls https://$HOSTNAME:2379 \
   --listen-client-urls https://$IP:2379,https://127.0.0.1:2379 \
   --listen-peer-urls https://$IP:2380
